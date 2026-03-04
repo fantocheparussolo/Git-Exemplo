59c28b35e4090351881e94c2de504e417ec25795
Vitor Parussolo <vitorparussolo@hotmail.com>
2026-03-04 15:42:09 -0300

    Teste

--- src/gitexemplo/GitExemplo.java
+++ src/gitexemplo/GitExemplo.java
@@ -3,7 +3,7 @@ package gitexemplo;
 public class GitExemplo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
     }
     
 }
